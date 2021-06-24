7b3f9a351e351c2eb1dbd621302dada9a165449e
Ayush Giri <63806638+Ayushrobben@users.noreply.github.com>
2021-06-24 16:06:16 +0530

    minor change

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -30,25 +34,65 @@ def generate_password():
     pyperclip.copy(password)
 
 
-
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password1 = password_entry.get()
+    new_data = {
+
+        website: {
+            "email": email,
+            "password": password1
+        }
+    }
 
     if len(website) == 0 or len(password1) == 0:
         messagebox.showerror("Error!!!", "please don't leave the fields empty")
     else:
+        try:
+            with open("data.json", "r") as data_file:
+                # reading_data
+                data = json.load(data_file)
+                # updating old data with new data
+                data.update(new_data)
 
-        is_ok = messagebox.askokcancel(title="Confirmation",
-                                       message=f"Email = {email}\n Password = {password1}\n click ok to save\n click cancel to go back and edit")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password1}\n")
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as datafile:
+                json.dump(new_data, datafile, indent=4)
+
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+def find_password():
+    website_to_searched = website_entry.get()
+    try:
+        with open("data.json", "r") as datafile:
+            data = json.load(datafile)
+    except FileNotFoundError:
+        messagebox.showinfo("Error", "No such file exist")
+    else:
+        if data[website_to_searched]:
+            user_email = data[website_to_searched]["email"]
+            user_password = data[website_to_searched]["password"]
+            messagebox.showinfo("Details",
+                                f'website = {website_to_searched}\nemail = {user_email},password = {user_password}')
+        else:
+            messagebox.showinfo("Error", "sorry no such credentials exist")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -83,6 +127,9 @@ password_entry.grid(column=1, row=3)
 
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3, columnspan=2)
+
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, columnspan=2)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
